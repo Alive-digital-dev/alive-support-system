fc33838b90dbd50d0398f3d6ba44281c7dbed458
Alive-digital-dev <aliveagency.marketing@gmail.com>
2025-07-04 19:50:07 +0300

    Create create-files.sh

---
+++ create-files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# יצירת המבנה של המערכת
+mkdir -p backend/websocket
+mkdir -p database
+mkdir -p admin
+mkdir -p portal
+mkdir -p chatbot
+mkdir -p uploads
+mkdir -p logs
+mkdir -p docker/apache
+mkdir -p scripts
+mkdir -p error-pages
+
+# [כאן יהיו כל הקבצים...]
